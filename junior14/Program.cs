53b231398e8646643ff2d445c0f87c0ad4990f7c
Zmeegolov <karantinz@mail.ru>
2023-04-10 18:42:12 +0300

    Добавьте файлы проекта.

---
+++ junior14/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+
+int length = name.Length;
+Console.WriteLine("Длина строки: " + length);
+
+Console.WriteLine("Введите символ");
+char symbol = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите ширину");
+int width = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите высоту");
+int height = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < height; i++)
+{
+	for (int j = 0; j < width; j++)
+	{
+		if (i == 0 || i == height - 1)
+		{
+            Console.Write(symbol);
+        }
+		else if (j == 0 || j == width - 1)
+		{
+            Console.Write(symbol);
+        }
+		else if (i == height / 2 && j == (width - length) / 2)
+		{
+			Console.Write(name);
+			j += length - 1;
+		}
+		else
+		{
+			Console.Write(" ");
+		}
+	}
+	Console.WriteLine();
+}
